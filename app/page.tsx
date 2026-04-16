28422057b62cde41d25a4317007ac1dd28295072
charmarketing <archivooculto80@gmail.com>
2026-04-16 19:51:40 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -928,7 +928,7 @@ useEffect(()=>{
           {render()}
         </main>
       </div>
-     {modalNuevoCliente && <ModalNuevoCliente c={c} theme={theme} clientes={clientes} setClientes={setClientes} setModalNuevoCliente={setModalNuevoCliente} COLORES_CLIENTE={COLORES_CLIENTE}/>}
+     {modalNuevoCliente && <ModalNuevoCliente c={c} theme={theme} clientes={clientes} setClientes={setClientes} setModalNuevoCliente={setModalNuevoCliente} COLORES_CLIENTE={COLORES_CLIENTE} sb={supabase}/>}
     </div>
   )
 }
