855e1dacb4984ec583ebafd4c34e204b14fc1141
charmarketing <archivooculto80@gmail.com>
2026-04-29 19:01:31 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: '500mb',
+    },
+    responseLimit: '500mb',
+  },
+}
+module.exports = nextConfig
